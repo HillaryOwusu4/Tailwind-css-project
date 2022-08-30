43a1d2986dcb403cf0e4b57ed9cc9c405ff7b612
Hillary Ameyaw Owusu <hillary.owusu@amalitech.org>
2022-08-30 03:06:32 -0700

    Looped through objects of arrays using mapping

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,17 @@
 const Banner = () => {
+
+    let banners = [{name:'John Smith',content:"Club Dj",info:"Performs in clubs and needs to know what is trending in the music industry",src:"./images/Adidas X Ninja - Time In.png"},{
+        name:'Connie Willson',content:"Mobile Dj",info:"Performs in clubs and needs to know what is trending in the music industry",src:"./images/Lucy-S-Magazine.png"
+    },{name:'Morris Watson',content:"Radio Dj",info:"Works on the radio neesd a fresh quality sound",src:"./images/3fea0d07-a683-4b36-8f17-cc3d02b180a8.png"}]
+    // let thats=""
+    //     banners.forEach(blogs => {
+    //          thats = blogs.name;
+    //          console.log(thats)
+    //     });
+     
+    
     return ( <div className="w-full h-screen bg-primary flex justify-center">
-<div className="w-4/5 h-2/3 -400 flex">
+<div className="w-4/5 h-2/3  flex">
     <div className="w-2/5 h-full flex justify-center">
         <img src="./images/MyMP3Pool__DJ_Music_Website_Design_Case_Study-removebg-preview.png"/>
     </div>
@@ -16,20 +27,23 @@ const Banner = () => {
             </div>
         </div>
         <div className="w-full h-56 grid grid-cols-3 gap-12 mt-20">
-<div className="flex flex-col  justify-center  ">
-    <img src="./images/Adidas X Ninja - Time In.png" alt="pic" className="w-16 h-16 rounded-full "/>
-    <span className="mt-4 text-white">John Smith</span>
-    <span className="mt-1 text-nude">Club Dj</span>
-    <span className="mt-2 text-nude text-sm ">Performs in clubs and needs to know what is trending in the music industry</span>
-</div>
-<div className="flex flex-col  justify-center"> <img src="./images/Lucy-S-Magazine.png" alt="pic" className="w-16 h-16 rounded-full object-cover"/>
+
+    {banners.map((bloogs) =>{
+        return (<div className="flex flex-col  justify-center">
+        <div className="flex flex-col  justify-center"> <img src={bloogs.src} alt="pic" className="w-16 h-16 rounded-full object-cover"/>
+         <span className="mt-4 text-white">{bloogs.name}</span>
+         <span className="mt-1 text-nude">{bloogs.content}</span>
+         <span className="mt-2 text-nude text-sm ">{bloogs.info}</span></div></div>)
+    })}
+
+{/* <div className="flex flex-col  justify-center"> <img src="./images/Lucy-S-Magazine.png" alt="pic" className="w-16 h-16 rounded-full object-cover"/>
     <span className="mt-4 text-white">Connie Willson</span>
     <span className="mt-1 text-nude">Mobile Dj</span>
     <span className="mt-2 text-nude text-sm ">Performs in clubs and must keep abrest of the latest trending in twon</span></div>
 <div className=" flex flex-col  justify-center mb-3"><img src="./images/3fea0d07-a683-4b36-8f17-cc3d02b180a8.png" alt="pic" className="w-16 h-16 rounded-full object-cover"/>
     <span className="mt-4 text-white">  Morris Watson</span>
     <span className="mt-1 text-nude">Radio Dj</span>
-    <span className="mt-2 text-nude text-sm ">Works on the radio neesd a fresh quality sound</span></div>
+    <span className="mt-2 text-nude text-sm ">Works on the radio neesd a fresh quality sound</span></div> */}
         </div>
     </div>
 </div>
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 const  Footer = () => {
+    const list = [{name:"Convenient Search with autocomplete tips"},{name:"Different ways to download a song"},{name:"Download whole playlist to a sing;e file"},{name:"Nice preview of songs and movies"},{name:"Different color scheme"},{name:"Properly taged songs"}]
     return (<div className="w-full h-screen bg-primary flex justify-center">
 
        <div className="w-4/5 h-full  flex">
@@ -8,16 +9,14 @@ const  Footer = () => {
         <div className="w-1/2 h-full">
             <div className="w-full h-40 flex flex-col justify-end text-3xl font-bold text-white border-b border-white"><span >Based on the survey  </span><span className="block">results we've made a list</span><span>of users need</span></div>
             <div className="h-3/5 w-full relative">
-                <ul className="mt-6">
+                {list.map((object)=>{
+                    return( <ul className="mt-6">
                     <li className="list-disc list-inside text-blue-400 mt-6">
-                        <span className="text-white">Convenient Search with autocomplete tips</span> </li>
-                       <li  className="list-disc mt-6 list-inside text-blue-400"> <span className="text-white">Different ways to download a song</span></li>
-                      <li  className="list-disc mt-6 list-inside text-blue-400">  <span className="text-white">Download whole playlist to a sing;e file</span></li>
-                        <li className="list-disc mt-6 list-inside text-blue-400"><span className="text-white">Nice preview of songs and movies</span></li>
-                       <li className="list-disc mt-6 list-inside text-blue-400"> <span className="text-white">Different color scheme</span></li>
-                      <li className="list-disc mt-6 list-inside text-blue-400">  <span className="text-white">Properly taged songs</span></li>
+                        <span className="text-white">{object.name}</span> </li>
                    
-                </ul>
+                </ul>)
+                })}
+               
             </div>
         </div>
 
